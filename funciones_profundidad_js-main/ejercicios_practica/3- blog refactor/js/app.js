741dc419cb5787226ebb30ef2c15dbe597a19c3f
Valeria Riveros <92410347+ValRiv@users.noreply.github.com>
2022-08-12 15:42:30 -0300

    Add files via upload

---
+++ funciones_profundidad_js-main/ejercicios_practica/3- blog refactor/js/app.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/* Renderizar datos */
+let accumulator = ""
+for(const post of data){
+
+  accumulator += `
+  <article class="post">
+    <div class="post-header">
+      <img
+      src= "${post.avatar}"
+        class="icon-avatar"
+        alt="avatar icon"
+        width="75"
+      />
+      <h2>${post.titulo}</h2>
+    </div>
+    <div>
+      <p>
+        ${post.texto}
+      </p>
+    </div>
+  </article>
+  `
+};
+
+const section = document.querySelector("section");
+section.innerHTML = accumulator;
+/* Agregar eventos */
+addPostMouseEvents();
